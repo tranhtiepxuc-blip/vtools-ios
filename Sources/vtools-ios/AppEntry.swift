fc303b4911a209863ae44cccdeb9c5082d928983
tranhtiepxuc-blip <tranhtiepxuc@gmail.com>
2026-08-02 12:13:17 +0700

    Update and rename main.swift to AppEntry.swift

---
+++ Sources/vtools-ios/AppEntry.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct VToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("vTools Loaded")
+                .font(.largeTitle)
+        }
+    }
+}
--- Sources/vtools-ios/main.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-// Nếu app dịch ngược dùng AppDelegate truyền thống:
-UIApplicationMain(
-    CommandLine.argc,
-    CommandLine.unsafeArgv,
-    nil,
-    NSStringFromClass(AppDelegate.self)
-)
